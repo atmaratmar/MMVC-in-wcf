9ac32992514c67ed6d6ae3ba29d6b2cd99b1fd7a
Atmar Kohistany <atma0004@edu.eal.dk>
2017-12-21 15:28:30 +0100

    Add project files.

---
+++ MMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MMVC.Models;
+
+namespace MMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public async Task<ActionResult> Index()
+        {
+            var service = new ServiceReference2.PersonWcfServiceClient();
+            var personer = await service.GetAllePersonerAsync();
+            return View(personer);
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MMVC/Controllers/PPPController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MMVC.Controllers
+{
+    public class PPPController : Controller
+    {
+       
+        // GET: PPP
+        public async Task<ActionResult> Index()
+        {
+            var service = new ServiceReference2.PersonWcfServiceClient();
+            var personer = await service.GetAllePersonerAsync();
+            return View(personer);
+        }
+
+        public async Task<ActionResult> Detail(int id)
+        {
+            var service = new ServiceReference2.PersonWcfServiceClient();
+            var personer = await service.GetAllePersonerAsync();
+            return View(personer[id]);
+        }
+        // GET: PPP/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+            
+        //}
+
+        // GET: PPP/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PPP/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PPP/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PPP/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PPP/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PPP/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
